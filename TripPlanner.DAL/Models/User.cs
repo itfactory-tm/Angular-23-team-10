be4f9e740d958ee8d47f844af7c66f7e4d8847e4
Michiel Dausy <r0900099@student.thomasmore.be>
2023-10-09 16:58:39 +0200

    Database aangemaakt

--- TripPlanner.DAL/Models/User.cs
+++ TripPlanner.DAL/Models/User.cs
@@ -5,7 +5,7 @@
         public int UserId { get; set; }
         public string Firstname { get; set; }
         public string Surname { get; set; }
-        /*        public string Email { get; set; }
+        /*      public string Email { get; set; }
                 public string Password { get; set; }
                 public Boolean IsAdmin { get; set; }
                 public bool IsActive { get; set; }*/
