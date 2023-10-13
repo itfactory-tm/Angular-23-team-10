d7ab366735b664afc5e099b6b1763af21dc10c97
michieldausy <michiel_dausy@hotmail.com>
2023-10-13 19:21:37 +0200

    Test pagina voor api gemaakt

--- TripPlanner/src/app/app-routing.module.ts
+++ TripPlanner/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TestAPIComponent } from './test-api/test-api.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }, {path: 'users', component: TestAPIComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TripPlanner/src/app/app.module.ts
+++ TripPlanner/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NavbarComponent, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, NavbarComponent, BrowserAnimationsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
