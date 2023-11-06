afbdecc5eee7efb7b70d9c45be1240aa0dc27764
michieldausy <michiel_dausy@hotmail.com>
2023-11-06 16:10:44 +0100

    removed user table

--- Back-end/TripPlannerAPI/Dto/User/UserUpdate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TripPlannerAPI.Dto.User
-{
-    public class UserUpdate
-    {
-        public int UserId { get; set; }
-        public bool IsAdmin { get; set; }
-        public bool IsActive { get; set; }
-    }
-}
