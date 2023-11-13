89983af7e0659035a94a8ab36a4cfd517d73c2f5
JonasBaelus <r0901372@student.thomasmore.be>
2023-11-13 16:21:42 +0100

    auth0 geimplementeerd

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ToastComponent } from './toast/toast.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthModule } from '@auth0/auth0-angular';
 import { AdminComponent } from './admin/admin.component';
+import { environment } from 'src/environments/environment';
+import { AuthHttpInterceptor } from '@auth0/auth0-angular';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent, AdminComponent],
@@ -18,22 +21,28 @@ import { AdminComponent } from './admin/admin.component';
     BrowserModule,
     AppRoutingModule,
     NavbarComponent,
+    FooterComponent,
     ToastComponent,
     FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     FontAwesomeModule,
     ReactiveFormsModule,
-
     AuthModule.forRoot({
-      domain: 'dev-4bvxqmetijxppw7f.us.auth0.com',
-      clientId: 'WFFRD0OARum3P9KftRFkf9Wodr144pWO',
+      //domain: 'dev-2ki8nim0a3vrbww1.us.auth0.com',
+      domain: environment.AUTH0_DOMAIN,
+      //clientId: 'jGOPQttdT71dyPnd7zJUepi46zGNoihE',
+      clientId: environment.AUTH0_CLIENT_ID,
       authorizationParams: {
-        redirect_uri: window.location.origin,
+        redirect_uri: window.location.origin
       },
+      // The AuthHttpInterceptor configuration
+      httpInterceptor: {
+        allowedList: [`${environment.api_url}/trip`,`${environment.api_url}/trip/*`]
+      }
     }),
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
