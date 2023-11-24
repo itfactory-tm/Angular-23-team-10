dcc0501698308ff11fd183d42bb63fc8f4c129f5
Elias Grinwis <r0892566@student.thomasmore.be>
2023-11-24 13:11:38 +0100

    Github action front-end

--- Front-end/src/environments/environment.ts
+++ Front-end/src/environments/environment.ts
@@ -1,17 +1,9 @@
 export const environment = {
-  production: false, // this is true in the production environment
-  home_url: 'https://localhost:4200',
+  production: true,
+  home_url: 'https://trip-planner-46730.web.app/',
   AUTH0_DOMAIN: 'dev-2ki8nim0a3vrbww1.us.auth0.com',
   AUTH0_CLIENT_ID: 'jGOPQttdT71dyPnd7zJUepi46zGNoihE',
-  AUTH0_AUDIENCE: 'https://localhost:7113',
-  redirectUri: 'https://localhost:4200/callback',
-  api_url: 'https://localhost:7113/api',
-
-  // production: true, // this is true in the production environment
-  // home_url: 'https://trip-planner-46730.web.app/',
-  // AUTH0_DOMAIN: 'dev-2ki8nim0a3vrbww1.us.auth0.com',
-  // AUTH0_CLIENT_ID: 'jGOPQttdT71dyPnd7zJUepi46zGNoihE',
-  // AUTH0_AUDIENCE: 'https://tripplanner-api-eliasgrinwis.cloud.okteto.net',
-  // redirectUri: 'https://trip-planner-46730.web.app/callback',
-  // api_url: 'https://tripplanner-api-eliasgrinwis.cloud.okteto.net/api',
+  AUTH0_AUDIENCE: 'https://tripplanner-api-eliasgrinwis.cloud.okteto.net',
+  redirectUri: 'https://trip-planner-46730.web.app/callback',
+  api_url: 'https://tripplanner-api-eliasgrinwis.cloud.okteto.net/api',
 };
