30dbeec021c2018f0d43af176da3364e17dbad25
JonasBaelus <r0901372@student.thomasmore.be>
2023-11-27 16:09:35 +0100

    audiences

--- Front-end/src/environments/environment.ts
+++ Front-end/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   home_url: 'https://trip-planner-46730.web.app/',
   AUTH0_DOMAIN: 'dev-2ki8nim0a3vrbww1.us.auth0.com',
-  AUTH0_CLIENT_ID: 'm1tdZcMxT1c0LLiIwvEz3fvhEBn7NPnv',
+  AUTH0_CLIENT_ID: 'jGOPQttdT71dyPnd7zJUepi46zGNoihE',
   AUTH0_AUDIENCE: 'https://tripplanner-api-eliasgrinwis.cloud.okteto.net',
   redirectUri: 'https://trip-planner-46730.web.app',
   api_url: 'https://tripplanner-api-eliasgrinwis.cloud.okteto.net/api',
