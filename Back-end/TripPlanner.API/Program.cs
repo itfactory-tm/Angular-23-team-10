9e47d93b03c55704c298edb3a03a6f7784d1e7e3
JannesVdB <r0835818@student.thomasmore.be>
2023-12-02 17:54:00 +0100

    updated urls

--- Back-end/TripPlanner.API/Program.cs
+++ Back-end/TripPlanner.API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: corsConfig,
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200", "https://trip-planner-46730.web.app")
             .AllowAnyMethod()
             .AllowAnyHeader();
         });
