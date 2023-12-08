c019ed0fa2cc4660ea24f7c2bc97ecaca17e4b90
michieldausy <michiel_dausy@hotmail.com>
2023-12-08 15:31:12 +0100

    pagination activity

--- Front-end/src/app/components/activity/activity.component.ts
+++ Front-end/src/app/components/activity/activity.component.ts
@@ -3,28 +3,33 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ActivityType } from 'src/app/models/ActivityType';
 import { ActivityTypeService } from 'src/app/services/activity-type/activity-type.service';
-import { Subscription } from 'rxjs';
+import { Subscription, of } from 'rxjs';
 import { PageLoaderComponent } from '../../shared/page-loader/page-loader.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 import { ActivityFormComponent } from '../activity-form/activity-form.component';
 import { ToastComponent } from '../../shared/toast/toast.component';
-import { SidebarComponent } from "../sidebar/sidebar.component";
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { PaginatedResult } from 'src/app/models/Pagination';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-activity-list',
-    standalone: true,
-    templateUrl: './activity.component.html',
-    styleUrls: ['./activity.component.css'],
-    imports: [
-        CommonModule,
-        PageLoaderComponent,
-        FontAwesomeModule,
-        ActivityFormComponent,
-        ToastComponent,
-        SidebarComponent
-    ]
+  selector: 'app-activity-list',
+  standalone: true,
+  templateUrl: './activity.component.html',
+  styleUrls: ['./activity.component.css'],
+  imports: [
+    CommonModule,
+    PageLoaderComponent,
+    FontAwesomeModule,
+    ActivityFormComponent,
+    ToastComponent,
+    SidebarComponent,
+    NgxPaginationModule,
+    FormsModule
+  ],
 })
 export class ActivityListComponent implements OnInit, OnDestroy {
   activities: ActivityType[] = [];
@@ -41,6 +46,11 @@ export class ActivityListComponent implements OnInit, OnDestroy {
   activityId: number = 0;
   isSubmitted: boolean = false;
 
+  searchName: string = '';
+  config: any;
+  pageSizes: number[] = [2, 4, 6, 8, 10];
+  selectedPageSize: number = 4;
+
   constructor(
     private activitTypeService: ActivityTypeService,
     private router: Router,
@@ -49,6 +59,11 @@ export class ActivityListComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.getActivities();
+    this.config = {
+      itemsPerPage: this.selectedPageSize,
+      currentPage: 1,
+      totalItems: 5,
+    };
   }
 
   ngOnDestroy(): void {
@@ -57,30 +72,45 @@ export class ActivityListComponent implements OnInit, OnDestroy {
   }
 
   delete(id: number) {
-    this.deleteActivity$ = this.activitTypeService.deleteActivityType(id).subscribe({
-      next: (v) => {
-        // Delete was successful, update activities and set mode to 'delete'
-        this.getActivities();
-        this.mode = 'delete';
-      },
-      error: (e) => {
-        // Handle error if deletion fails
-        this.errorMessage = e.message;
-      },
-    });
-  }
-  
-
-  getActivities() {
-    this.isLoading = true;
-    this.activities$ = this.activitTypeService
-      .getActivityTypes()
-      .subscribe((result) => {
-        (this.activities = result), (this.isLoading = false);
+    this.deleteActivity$ = this.activitTypeService
+      .deleteActivityType(id)
+      .subscribe({
+        next: (v) => {
+          // Delete was successful, update activities and set mode to 'delete'
+          this.getActivities();
+          this.mode = 'delete';
+        },
+        error: (e) => {
+          // Handle error if deletion fails
+          this.errorMessage = e.message;
+        },
       });
   }
 
-  
+  getActivities(pageNumber: number = 1) {
+    this.isLoading = true;
+    this.activities$ = this.activitTypeService
+      .getActivityTypes(
+        this.searchName.trim(),
+        pageNumber,
+        this.selectedPageSize
+      )
+      .subscribe(
+        (response: PaginatedResult<ActivityType[]>) => {
+          this.activities = response.result;
+          this.config = {
+            itemsPerPage: this.selectedPageSize,
+            currentPage: response.pagination.CurrentPage,
+            totalItems: response.pagination.TotalItems,
+          };
+          this.isLoading = false;
+        },
+        (error) => {
+          return of(null);
+        }
+      );
+  }
+
   sort(filter: string) {
     if (filter !== this.currentSort) {
       this.currentSort = filter;
@@ -89,14 +119,14 @@ export class ActivityListComponent implements OnInit, OnDestroy {
       .getActivityTypes()
       .subscribe((result) => {
         if (filter == 'name') {
-          this.activities = result.sort((a, b) => a.name.localeCompare(b.name));
+          this.activities = result.result.sort((a, b) => a.name.localeCompare(b.name));
         } else {
-          this.activities = result.sort();
+          this.activities = result.result.sort();
         }
         this.isLoading = false;
       });
   }
-  
+
   openModal(mode: string, id: number) {
     this.isSubmitted = false;
     this.mode = mode;
@@ -109,5 +139,22 @@ export class ActivityListComponent implements OnInit, OnDestroy {
   onUpdateOrCreate() {
     this.isSubmitted = true;
     this.getActivities();
+  }
+
+  pageChanged(event: number) {
+    this.getActivities(event);
+  }
+
+  submit(search?: string, ids?: number[]) {
+    if (search !== undefined) {
+      this.searchName = search;
+    }
+
+    this.getActivities();
+  }
+
+  changePageSize(event: number) {
+    this.selectedPageSize = event;
+    this.getActivities()
   }
 }
